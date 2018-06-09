542d936e7ed741ff38de152a4a3b10c917ea726b
Youssef Khatib <40075290+AslanGit@users.noreply.github.com>
2018-06-09 14:35:14 +0200

    New ShipPrefab

--- Pannkakor/Assets/Code/Shipmovement.cs
+++ Pannkakor/Assets/Code/Shipmovement.cs
@@ -9,28 +9,39 @@ public class Shipmovement : MonoBehaviour {
     float accelerationForce = 10f;
     float rotationForce = 100f;
     public GameObject bullet;
-
-    // Use this for initialization
+    public Rigidbody2D rb;
     void Start () {
 
-
+       rb = GetComponent<Rigidbody2D>();
 
     }
 	
-	// Update is called once per frame
-	void FixedUpdate () {
-        transform.Rotate(0, 0, -Input.GetAxis("Horizontal") * rotationForce * Time.deltaTime);
 
-        GetComponent<Rigidbody2D>()
-            .AddForce(transform.up * accelerationForce * Input.GetAxis("Vertical"));
+	void FixedUpdate () {
+
+        transform.Rotate(0, 0, -Input.GetAxis("Horizontal") * rotationForce * Time.deltaTime);
+            
+        rb.AddForce(transform.up * accelerationForce * Input.GetAxis("Vertical"));
 
         if (Input.GetMouseButtonDown(0))
             ShootBullet();
+
+
     }
 
     void ShootBullet()
     {
         Instantiate(bullet, new Vector3(transform.position.x, transform.position.y, 0),
             transform.rotation);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+           rb.velocity = new Vector3(0, 0, 0);
+           
+           
+
+
     }
 }
