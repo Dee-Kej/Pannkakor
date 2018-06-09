2c0433da1f8560ddc49ef9245868abe6a7d70037
DEMOLITION\Dee-Kej <daniel_kk9012@hotmail.com>
2018-06-09 12:20:50 +0200

    making borders 2D

--- Pannkakor/Assets/Code/BorderWall.cs
+++ Pannkakor/Assets/Code/BorderWall.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BorderWall : MonoBehaviour {
 
-    void OnTriggerEnter2D(Collider collider)
+    void OnTriggerEnter2D(Collider2D collider)
     {
         Debug.Log(collider.gameObject.name);
         Destroy(collider.gameObject); 
