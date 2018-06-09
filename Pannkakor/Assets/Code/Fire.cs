6b5b20df525e2e2a288db009dd88bf4557f8b6f6
DEMOLITION\Dee-Kej <daniel_kk9012@hotmail.com>
2018-06-09 11:27:35 +0200

    Added bullet code, Fire code, and Border code. Playing around in DKScene

---
+++ Pannkakor/Assets/Code/Fire.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour {
+
+    public GameObject bulletObject;
+
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Instantiate(bulletObject, this.transform);
+        }
+	}
+}
