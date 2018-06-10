1a29612fb90c7eed33edc9fd7bbe6219312acbe9
DEMOLITION\Dee-Kej <daniel_kk9012@hotmail.com>
2018-06-10 09:58:40 +0200

    Resetting playercount when restarting from main game with Escape

--- Pannkakor/Assets/Code/RestartGame.cs
+++ Pannkakor/Assets/Code/RestartGame.cs
@@ -5,10 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class RestartGame : MonoBehaviour {
 
+    GameObject playerData;
+
+    void Start()
+    {
+        if (GameObject.Find("Persistent PlayerData") == true)
+        {
+            playerData = GameObject.Find("Persistent PlayerData");
+        }
+    }
+
 	void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
+            playerData.GetComponent<PlayerCount>().player1 = false;
+            playerData.GetComponent<PlayerCount>().player2 = false;
+            playerData.GetComponent<PlayerCount>().player3 = false;
+            playerData.GetComponent<PlayerCount>().player4 = false;
             SceneManager.LoadScene(0);
         }
     }
