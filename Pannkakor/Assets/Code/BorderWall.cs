bbc824389d6b05f20a1a70b3ef2caf17f5710d25
DEMOLITION\Dee-Kej <daniel_kk9012@hotmail.com>
2018-06-10 11:53:56 +0200

    adding bouncewall to maingame, and updating small things

--- Pannkakor/Assets/Code/BorderWall.cs
+++ Pannkakor/Assets/Code/BorderWall.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class BorderWall : MonoBehaviour {
 
-    void OnTriggerEnter2D(Collider2D collider)
-    {
-        //Debug.Log(collider.gameObject.name);
-        Destroy(collider.gameObject); 
-    }
+    //void OnTriggerEnter2D(Collider2D collider)
+    //{
+    //    Debug.Log(collider.gameObject.name);
+    //    Destroy(collider.gameObject);
+    //}
 }
