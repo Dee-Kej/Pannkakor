e0e6196abf71c6da1dc4f53361adcc80ebe6100a
DEMOLITION\Dee-Kej <daniel_kk9012@hotmail.com>
2018-06-10 13:01:01 +0200

    New bullets and new mainmenu pictures implemented

---
+++ Pannkakor/Assets/Code/GameScenarios.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScenarios : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if players all die, aliens win
+
+        //if one player is left, that player wins, and we prompt a restart of the game
+
+
+	}
+}
